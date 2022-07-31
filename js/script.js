64eeab04b1b423d2321e81255a8cba42a2f5c1a9
Irina <irinkapap@gmail.com>
2022-07-31 21:53:29 +0200

    practice 3

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ const personalMovieDB = {
     privat: false
 };
 
-
-for (let i = 0; i < 2; i++) {
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
         b = prompt('На сколько оцените его?', '');
     
@@ -35,18 +35,39 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 
-    
+    }
 }
 
-if (personalMovieDB.count < 10) {
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов' );
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if ( personalMovieDB.count >= 30 ) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if ( personalMovieDB.count >= 30 ) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
 }
 
-console.log(personalMovieDB);
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {        
+        personalMovieDB.genre[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+    
+
 
